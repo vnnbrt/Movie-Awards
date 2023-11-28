ebbb0200c5237dd7b2a64b8cc11b8cd0c3cf76a5
vnnbrt <vinniebritos@gmail.com>
2023-11-28 00:30:48 -0300

    consumindo api

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
-// Alerta da página sem dados
+// Alerta da página sem dados //
 
 function alerta() {
     alert("Esta página ainda não foi criada e não contém dados.");
 }
 
-// Função do botão + em cast e crew
+// Função do botão + em cast e crew //
 
 function myFunction() {
     let dots = document.getElementById("dots");
@@ -21,3 +21,25 @@ function myFunction() {
       moreText.style.display = "inline";
     }
 }
+
+// API filme TETLAMT //
+
+let tituloOriginal = document.getElementById("tituloOriginal");
+let notaImdb = document.getElementById("notaImdb");
+let anoFilme = document.getElementById("anoFilme");
+let genero = document.getElementById("genero");
+let duração = document.getElementById("duração");
+let sinopseFilme = document.getElementById("sinopseFilme");
+
+fetch("https://www.omdbapi.com/?i=tt6710474&apikey=c0874d8f")
+  .then((resp) => resp.json ())
+  .then((dados) => {
+    console.log((dados));
+
+    tituloOriginal.innerHTML = "Título original: " + dados.Title;
+    anoFilme.innerHTML = dados.Year;
+    genero.innerHTML = dados.Genre;
+    duração.innerHTML = dados.Runtime;
+    notaImdb.innerHTML = "Avaliação Imdb: " + dados.imdbRating + "/10"; 
+    sinopseFilme.innerHTML = dados.Plot;
+    });
